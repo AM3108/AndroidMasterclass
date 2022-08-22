2be4b6edac516cbdb348a201ffd11714a2374634
Atul <atulsmart1996@gmail.com>
2022-08-22 20:30:30 +0530

    Commit with navigation_architecture_component

---
+++ app/src/main/java/com/atul/androidmasterclass/exercise/ExerciseActivity.kt
@@ -0,0 +1,12 @@
+package com.atul.androidmasterclass.exercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.atul.androidmasterclass.R
+
+class ExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+    }
+}
